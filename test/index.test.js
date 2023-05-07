3f82328a8321b6a548acc295b1992642404bae0f
Jose Pena <jose.pena@ibm.com>
2023-05-07 18:29:05 -0300

    Third test case created:
    Should return true when the number 25 is passed in the parameter

--- test/index.test.js
+++ test/index.test.js
@@ -8,4 +8,8 @@ describe('squares', () => {
     it('Should return true when the number 4 is passed in the parameter', () => {
         expect(squares(4)).toBe(true);
     });
+
+    it('Should return true when the number 25 is passed in the parameter', () => {
+        expect(squares(25)).toBe(true);
+    });
 });
