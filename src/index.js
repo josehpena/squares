a7298933ca2f28310090b42c25bee777401565a3
Jose Pena <jose.pena@ibm.com>
2023-05-07 18:47:47 -0300

    just to check up test cases passed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const squares = (number) =>{
-    let counter = 0;
+    var counter = 0;
 
-    for(let i=0; counter < number; i++){
+    for(let i=0; counter <= number; i++){
 
     counter = (i * i);
 
     if(counter == number)
         return true;
     }
+    
     return false;
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -12,4 +12,12 @@ describe('squares', () => {
     it('Should return true when the number 25 is passed in the parameter', () => {
         expect(squares(25)).toBe(true);
     });
+
+    it('Should return false when the number 26 is passed in the parameter', () => {
+        expect(squares(26)).toBe(false);
+    });
+
+    it('Should return true when the number 0 is passed in the parameter', () => {
+        expect(squares(0)).toBe(true);
+    });
 });
